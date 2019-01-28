264190e0083d733d6400d2ffb07a9d35a289abf0
amir-amini <amirhossein.lmpd@yahoo.com>
2019-01-28 22:05:53 +0330

    shekarchi

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	int BulletLifeTime = 1;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		rb2d.velocity = new Vector2(10, 10);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		BulletLifeTime --;
+		rb2d.velocity = new Vector2(10, 5);
+		if (BulletLifeTime == 0)
+		{
+			Destroy(gameObject);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Hunter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter : MonoBehaviour {
+	float fireBulletRate = 1f;
+	public GameObject BulletPrefab;
+	float timeSinceLastShot = 0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastShot += Time.deltaTime;
+		if (fireBulletRate < timeSinceLastShot){
+				timeSinceLastShot = 0;
+				ShootBullet();
+			}
+}
+void ShootBullet(){
+
+	GameObject Bullet = Instantiate(BulletPrefab);
+	Bullet.transform.position = new Vector2(gameObject.transform.position[0]+0.60f,gameObject.transform.position[1]+0.35f);
+
+	// Debug.LogError(gameObject.transform.position[0]+gameObject.transform.position[1]);
+}
+}
